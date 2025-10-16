a63a6f3e665d4107e4393dc729a9e47710169692
JoseOtavio-Miguel <=>
2025-10-15 23:15:39 -0300

    Add detailed Pokémon info to show page

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -25,6 +25,34 @@ pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
         .then((response) => response.json())
         .then(convertDetailToPokemon)
+        .then(async (detailPokemon) => {
+
+            // Get Gender
+            const genderData = await pokeApi.getPokemonGender(detailPokemon);
+            const rate = genderData.gender_rate;
+
+            if(rate === -1) {
+                detailPokemon.gender = 'Genderless';
+            }
+            else {
+                const femalePercent = (rate / 8) * 100;
+                const malePercent = 100 - femalePercent;
+                detailPokemon.gender =`♂️ ${malePercent.toFixed(1)}% / ♀️ ${femalePercent.toFixed(1)}%`;
+            }
+
+            // Get Egg Groups
+            const eggData = await pokeApi.getPokemonEggGroup(detailPokemon);
+            detailPokemon.egg_groups = eggData.egg_groups;
+
+
+            // Get Status
+            const pokeStats = await pokeApi.getPokemonStats(detailPokemon);
+            detailPokemon.height = pokeStats.height;
+            detailPokemon.weight = pokeStats.weight;
+            detailPokemon.abilities = pokeStats.abilities;
+
+            return detailPokemon;
+        })
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
@@ -36,4 +64,41 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail)) 
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
+}
+
+
+pokeApi.getPokemonGender = (pokemon) => {
+    const genderUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemon.name}`;
+    return fetch(genderUrl)
+    .then(response => response.json())
+    .then(data => {
+        return {
+            gender_rate: data.gender_rate
+        };
+    });
+};
+
+
+pokeApi.getPokemonEggGroup = (pokemon) => {
+    const eggGroupUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemon.name}`;
+    return fetch(eggGroupUrl)
+    .then(response => response.json())
+    .then(data => {
+        return {
+            egg_groups: data.egg_groups.map(group => group.name)
+        }
+    })
+}
+
+pokeApi.getPokemonStats = (pokemon) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`;
+    return fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            return {
+                height: data.height / 10,
+                weight: data.weight / 10,
+                abilities: data.abilities.map(a => a.ability.name)
+            }
+        })
 }
